0b0c33d13970ecd8326cb593513678aa3e754dbf
LiaoRui <1405613785@qq.com>
2024-04-01 23:49:25 +0800

    功能五

--- src/views/newPage.vue
+++ src/views/newPage.vue
@@ -2,5 +2,8 @@
   <div class="newPage">
     <h1>This is an newPage page</h1>
     <h1>This is an newPage page</h1>
+    <h1>第五次 page5</h1>
+    <h1>第五次 page5</h1>
+    <h1>第五次 page5</h1>
   </div>
 </template>
