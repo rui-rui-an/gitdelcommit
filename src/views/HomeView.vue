887807ae038f6c97254e1a4de48b5449e49cf104
LiaoRui <1405613785@qq.com>
2024-04-01 22:18:39 +0800

    feat:功能三完成

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,6 +5,12 @@
     <el-button type="primary">element的按钮1</el-button>
     <el-button type="primary">element的按钮2</el-button>
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>功能33333333333333</div>
+    <div>功能33333333333333</div>
+    <div>功能33333333333333</div>
+    <div>功能33333333333333</div>
+    <div>功能33333333333333</div>
+    <div>功能33333333333333</div>
   </div>
 </template>
 
